b8f5fec3c6f1e9a80b0e43b2a98d1dcd41973a86
zhenhaoyun <zhenhaoyun1213@hotmail.com>
2019-02-23 13:54:04 +0800

    modified:   LICENSE.txt 	modified:   script.vmcustom_thesisprinting.php 	modified:   vmcustom_thesisprinting.xml

--- script.vmcustom_thesisprinting.php
+++ script.vmcustom_thesisprinting.php
@@ -5,8 +5,8 @@
  * @package		Joomla
  * @author		ekerner@ekerner.com
  * @website		http://www.ekerner.com
- * @license		Copyright (C) 2018 Gold Creative Services All rights reserved.
- * @copyright		Copyright (C) 2018 Gold Creative Services All rights reserved.
+ * @license		Copyright (C) 2019 Creative Momentum All rights reserved.
+ * @copyright		Copyright (C) 2019 Creative Momentum All rights reserved.
  */
 
 defined('_JEXEC') or die('Restricted access');
@@ -15,7 +15,6 @@ $cwdpath = dirname(__FILE__);
 $compath = is_dir($cwdpath . '/admin/helpers') ? $cwdpath . '/admin' : 
 	JPATH_ADMINISTRATOR . '/components/com_vmcustom_thesisprinting';
 require_once $compath . '/helpers/vmcustom_thesisprintingHelper.php';
-require_once $compath . '/helpers/File_CSV_DataSource.php';
 
 class com_vmcustom_thesisprintingInstallerScript
 {
@@ -34,52 +33,6 @@ class com_vmcustom_thesisprintingInstallerScript
 
 	public function install($adapter)
 	{
-		// create institutes db table ...
-		/*$institutes_table = "{$this->dbprefix}vmcustom_thesisprinting_institutes";
-		if (!$this->tableExists($institutes_table)) {
-			echo("Creating {$institutes_table} database table:<br>\n");
-			$this->db->setQuery("
-				CREATE TABLE {$institutes_table} (
-					institute_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-					logo1_id INT(1) UNSIGNED,
-					FOREIGN KEY (logo1_id) REFERENCES {$this->dbprefix}virtuemart_medias(virtuemart_media_id) ON DELETE SET NULL,
-					logo2_id INT(1) UNSIGNED,
-					FOREIGN KEY (logo2_id) REFERENCES {$this->dbprefix}virtuemart_medias(virtuemart_media_id) ON DELETE SET NULL,
-					institute VARCHAR(256) NOT NULL,
-					CONSTRAINT UNIQUE (institute),
-					color1 VARCHAR(32) DEFAULT '#000000',
-					color2 VARCHAR(32) DEFAULT '#111111',
-					spine_layout TINYINT DEFAULT 1
-				) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_general_ci
-			");
-			vmcustom_thesisprintingHelper::stupidJ3CatchDatabaseExecute($this->db, 'query');
-			if (vmcustom_thesisprintingHelper::reportIfDbError()) {
-				echo("Failed!<br><br>\n");
-				return false;
-			}
-			echo("OK!<br><br>\n");
-		}
-*/
-		// create courses db table ...
-		/*$courses_table = "{$this->dbprefix}vmcustom_thesisprinting_courses";
-		if (!$this->tableExists($courses_table)) {
-			echo("Creating {$courses_table} database table:<br>\n");
-			$this->db->setQuery("
-				CREATE TABLE {$courses_table} (
-					course_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-					course_title VARCHAR(256) NOT NULL,
-					CONSTRAINT UNIQUE (course_title),
-					course_desc VARCHAR(256)
-				) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_general_ci
-			");
-			vmcustom_thesisprintingHelper::stupidJ3CatchDatabaseExecute($this->db, 'query');
-			if (vmcustom_thesisprintingHelper::reportIfDbError()) {
-				echo("Failed!<br><br>\n");
-				return false;
-			}
-			echo("OK!<br><br>\n");
-		}*/
-
 		// TODO: create virtuemart products and variants if not exists - as per thesis printing requirements (specified)
 
 		return true;
@@ -92,11 +45,7 @@ class com_vmcustom_thesisprintingInstallerScript
  
 	public function uninstall($adapter)
 	{
-		// drop tables ...
-		/*$tables = array(
-			'vmcustom_thesisprinting_courses',
-			'vmcustom_thesisprinting_institutes'
-		);
+		$tables = [];
 		foreach ($tables as $table) {
 			echo("Removing {$this->dbprefix}{$table} database table:<br>\n");
 			$this->db->setQuery("DELETE IGNORE FROM {$this->dbprefix}{$table}");
@@ -104,7 +53,7 @@ class com_vmcustom_thesisprintingInstallerScript
 			$this->db->setQuery("DROP TABLE IF EXISTS {$this->dbprefix}{$table}");
 			vmcustom_thesisprintingHelper::stupidJ3CatchDatabaseExecute($this->db, 'query', true);
 			echo("OK!<br><br>\n");
-		}*/
+		}
 	}
 
 	private function tableExists($table_name)
